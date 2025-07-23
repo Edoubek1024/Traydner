0faefbd3b6853793c77061ae5ef0c2133caec3f0
Evan Doubek <doubek.evan@gmail.com>
2025-07-22 23:10:54 -0500

    Setup basic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import CreateAccount from "./pages/Auth/CreateAccount";
+import Home from "./pages/Home";
+import CreateAccount from "./pages/Auth/CreateAccount";
 import LandingPage from "./pages/Landing";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebaseConfig";
+import NavBar from "./components/NavBar";
+import NavBarDemo from "./components/NavBarDemo";
+import NotFound from "./pages/Error/404";
+import Login from "./pages/Auth/Login";
 
 function App() {
 
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <Router>
+      {user ? <NavBar /> : <NavBarDemo />}
+
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/*
         <Route path="/Home" element={<Home />} />
         <Route path="/CreateAccount" element={<CreateAccount />} />
-        */}
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path="/Login" element={<Login />} />
+        
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
