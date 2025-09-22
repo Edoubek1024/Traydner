53ed61b59905f2ba024b84662b91390234cd4197
Evan Doubek <doubek.evan@gmail.com>
2025-09-22 02:27:37 -0500

    Change origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,7 +51,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://www.traydner.com",
+        "https://traydner.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
